9c179706abf7cdeee8e93625426aefd211ef11ca
Den Marshall <dank03052005@gmail.com>
2023-09-23 16:58:40 +0300

    Added: vite-pwa setup in vite config.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'prompt',
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: 'CHANGETHIS',
+        short_name: 'CHANGETHIS',
+        description: 'CHANGETHIS',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+});
