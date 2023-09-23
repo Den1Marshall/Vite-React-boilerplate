5f9cee8e5d55686acc82ed39083cca8904e51431
Den Marshall <dank03052005@gmail.com>
2023-09-23 17:04:00 +0300

    Added: Layout & Home routing. Added: theme provider with auto-detecting user color mode. Added: updateSW func only for prod use.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
 import { FC } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Layout from './layouts/Layout';
+import Home from './pages/Home';
+import { ThemeProvider, useMediaQuery } from '@mui/material';
+import { darkTheme, lightTheme } from './theme/theme';
+// import updateSW from './utils/updateSW';
 
 const App: FC = () => {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+  // updateSW();
+
   return (
-    // <BrowserRouter>
-    // <Routes>
-    //   <Route path='/' element={Layout}>
-    //     <Route index element={Home page} />
-    //   </Route>
-    // </Routes>
-    // </BrowserRouter>
+    <ThemeProvider theme={prefersDarkMode ? darkTheme : lightTheme}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Layout />}>
+            <Route index element={<Home />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 };
 
